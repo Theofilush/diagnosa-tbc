c2144d2044526536d2bad5e1312265d36b2832d4
Rio Ariel Nugroho <rioariel98@gmail.com>
2019-12-05 13:27:57 +0700

    update project by rio

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,20 +25,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::define('manage-users', function($user){
-            return count(array_intersect(["ADMIN"], json_decode($user->roles)));
-        });
-
-        Gate::define('manage-categories', function($user){
-            return count(array_intersect(["ADMIN", "STAFF"], json_decode($user->roles)));
-        });
-
-        Gate::define('manage-books', function($user){
-            return count(array_intersect(["ADMIN", "STAFF"], json_decode($user->roles)));
-        });
-
-        Gate::define('manage-orders', function($user){
-            return count(array_intersect(["ADMIN", "STAFF"], json_decode($user->roles)));
-        });
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,6 @@
 |
 */
 
-Route::get('/', function(){
-    return view('auth.login');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Auth::routes();
-Route::match(["GET", "POST"], "/register", function(){
-    return redirect("/login");
-})->name("register");
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::resource("users", "UserController");
-
-Route::get('/categories/trash', 'CategoryController@trash')->name('categories.trash');
-Route::get('/categories/{id}/restore', 'CategoryController@restore')->name('categories.restore');
-Route::delete('/categories/{category}/delete-permanent', 'CategoryController@deletePermanent')->name('categories.delete-permanent');
-Route::resource('categories', 'CategoryController');
-
-Route::get('/ajax/categories/search', 'CategoryController@ajaxSearch');
-
-Route::get('/books/trash', 'BookController@trash')->name('books.trash');
-Route::post('/books/{book}/restore', 'BookController@restore')->name('books.restore');
-Route::delete('/books/{id}/delete-permanent', 'BookController@deletePermanent')->name('books.delete-permanent');
-Route::resource('books', 'BookController');
-
-Route::resource('orders', 'OrderController');
